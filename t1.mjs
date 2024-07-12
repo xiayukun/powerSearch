25e7a0a08b52c1757025e9d0f74de149e3927a00
夏宇坤 <xiayukuu@163.com>
2024-07-12 17:13:30 +0800

    s

--- t1.mjs
+++ t1.mjs
@@ -7,6 +7,6 @@ const AppSecret = '489445659d7f2d342641256d31cda060'
 const accessTokenUrl = `https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=${appid}&secret=${AppSecret}`
 axios.get(accessTokenUrl).then((response) => {
 	const access_token = response.data.access_token
-	$log('获取的access_token', access_token)
+	$log('获取的access_token', response.data)
 	debugger
 })
