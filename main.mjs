68f52f73e0b550c7b17102bc2e1e940ebdc71942
夏宇坤 <xiayukuu@163.com>
2024-07-12 22:01:14 +0800

    sdf

--- main.mjs
+++ main.mjs
@@ -1,17 +1,23 @@
 import './src/util/util.mjs'
 import express from 'express'
 import crypto from 'crypto'
+import xmlparser from 'express-xml-bodyparser'
 import xml2js from 'xml2js'
+// 用于解析XML格式的请求和回复消息
+const builder = new xml2js.Builder({ rootName: 'xml', headless: true })
+const parser = new xml2js.Parser({ explicitArray: false, explicitRoot: false })
 
 const app = express()
 const PORT = 16853
-// 创建一个解析器
-const parser = new xml2js.Parser({ explicitArray: false, explicitRoot: false })
+
+app.use(express.json())
+app.use(express.urlencoded())
+app.use(xmlparser())
 
 // 创建一个中间件来打印请求链接和请求头
 app.use(async (req, res, next) => {
 	// 打印请求的全路径
-	$log(`请求来了: ${req.protocol}://${req.get('host')}${req.originalUrl}`)
+	$log(`请求来了: ${req.protocol}://${req.get('host')}${req.originalUrl}`, req.body)
 	next() // 继续处理请求
 })
 app.get('/', (req, res) => {
@@ -32,20 +38,29 @@ app.get('/', (req, res) => {
 })
 app.post('/*', (req, res) => {
 	$log('接收到post请求')
-	$log('content-type', req.headers['content-type'])
-	if (req.headers['content-type'] === 'text/xml') {
-		// 解析XML请求体
-		parser.parseString(req.body, (err, result) => {
-			if (err) {
-				// 解析错误处理
-				$log('Error parsing XML:', err)
-				return res.status(500).send('Error parsing XML')
-			}
-			// 将解析后的数据添加到req对象中，以便后续处理
-			$log('接收到的消息为：', result)
-		})
-	}
 	// 处理请求...
+	if (req.body && req.body.xml) {
+		const xml = req.body.xml
+		if (xml.msgtype && xml.msgtype[0] === 'text') {
+		}
+	}
+	$log(req.body.xml.content[0])
+	// parser.parseString(res.body, (_err, result) => {
+	// 	// if (err) {
+	// 	// 	return res.status(500).send('Error parsing XML')
+	// 	// }
+	// 	$log('_err', _err)
+	// 	$log('result', result)
+	// 	// const reply = builder.buildObject({
+	// 	// 	xml: {
+	// 	// 		ToUserName: result.xml.FromUserName,
+	// 	// 		FromUserName: result.xml.ToUserName,
+	// 	// 		CreateTime: new Date().getTime(),
+	// 	// 		MsgType: 'text',
+	// 	// 		Content: '感谢您的消息！' // 这里是回复给用户的消息内容
+	// 	// 	}
+	// 	// })
+	// })
 	res.send('返回请求')
 })
 
